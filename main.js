3bf230a42df705c07e86ccfe6e8acd38ca9d65e9
“Kyle <kylemboomer@gmail.com>
2024-04-04 10:25:21 -0500

    Establish Food Arrays

---
+++ main.js
@@ -0,0 +1,20 @@
+var mainDishes = [
+    "Spaghetti Carbonara", "Chicken Alfredo", "Beef Stroganoff", "Vegetable Stir Fry", "Grilled Salmon",
+    "Mushroom Risotto", "Tacos", "Chicken Parmesan", "Taquitos", "Shrimp Scampi", "Pad Thai",
+    "Lamb Curry", "Pasta Primavera", "Hamburger", "Roast Chicken", "Vegetable Curry",
+    "Fish and Chips", "Lasagna", "Beef Burger", "Sushi"
+]
+
+var sideDishes = [
+    "Mashed Potatoes", "Steamed Vegetables", "Garlic Bread", "Caesar Salad", "Fried Rice", "Coleslaw",
+"Macaroni and Cheese", "Roasted Vegetables", "French Fries", "Onion Rings", "Baked Beans",
+"Quinoa Salad", "Caprese Salad", "Corn on the Cob", "Sweet Potato Fries", "Bruschetta", 
+"Green Bean Casserole", "Garlic Mashed Cauliflower", "Creamed Spinach", "Greek Salad"
+]
+
+var desserts = [
+    "Chocolate Cake", "Apple Pie", "Cheesecake", "Ice Cream Sundae", "Brownies", "Tiramisu", "Pumpkin Pie", 
+    "Lemon Tart", "Creme Brulee",  "Key Lime Pie", "Chocolate Chip Cookies", "Banana Split", 
+    "Strawberry Shortcake", "Blueberry Cobbler", "Red Velvet Cupcakes", "Peach Crisp",
+    "Carrot Cake", "Raspberry Cheesecake", "Pavlova", "Chocolate Mousse"
+]
